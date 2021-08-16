3d1dad0647ba39f68866d0f7715828890c91ce38
Juan JosÃ© Mangado Esteves <juan.mangado@fing.edu.uy>
2021-08-16 20:13:40 -0300

    Upload New File

---
+++ entrenamos.uy/src/logica/IActividadDeportivaController.java
@@ -0,0 +1,24 @@
+package logica;
+
+import java.util.Set;
+
+import datatypes.DtActividadDeportiva;
+import datatypes.DtClaseExt;
+import datatypes.TReg;
+
+public interface IActividadDeportivaController {
+	
+	public Set<String> obtenerInstituciones(); 
+	
+	public Boolean ingresarDatosActividadDep(String nombreInsti, DtActividadDeportiva datosAD);
+	
+	public /*DtActividadDeportiva,*/ Set<Set<String>> seleccionarActividadDeportiva(String ins, String nActDep);
+	
+	public Set<String> obtenerDeltaInstituciones(String nombreCup, String ins);
+	
+	public DtClaseExt seleccionarClase(String  ins, String actDep, String clase);
+	
+	public Set<String> obtenerSocios();
+	
+	public int inscribirSocio(String ins, String actDep, String clase, String socio, TReg tipoRegistro);
+}
