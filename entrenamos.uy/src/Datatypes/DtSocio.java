f0a56c2c494c7a42c42ec0115ea8f928030419b0
Juan JosÃ© Mangado Esteves <juan.mangado@fing.edu.uy>
2021-08-19 19:46:28 -0300

    Upload New File

---
+++ entrenamos.uy/src/Datatypes/DtSocio.java
@@ -0,0 +1,8 @@
+package datatypes;
+
+public class DtSocio extends DtUsuario {
+
+	public DtSocio(String nickname, String nombre, String apellido, String email, DtFecha fechaNacimiento) {
+		super(nickname, nombre, apellido, email, fechaNacimiento);
+	}
+}
