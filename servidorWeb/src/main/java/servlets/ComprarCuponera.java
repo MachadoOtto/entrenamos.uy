b2f48c240f27e62c262efeb6a4433fb2028975cf
Juan JosÃ© Mangado Esteves <juan.mangado@fing.edu.uy>
2021-10-09 18:21:50 -0300

    Replace ComprarCuponera.java

--- servidorWeb/src/main/java/servlets/ComprarCuponera.java
+++ servidorWeb/src/main/java/servlets/ComprarCuponera.java
@@ -37,13 +37,11 @@ public class ComprarCuponera extends HttpServlet {
     }    
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		processRequest(request, response);
 	}
 
 	public DtFecha getFechaActual() {
