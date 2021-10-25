bd43c2b57ff989a5dbb8643f3b3e40897cd76fc7
Guillermo Daniel Toyos Marfurt <guillermo.toyos@fing.edu.uy>
2021-10-24 21:26:47 -0300

    Cambios visuales 🤭🤭

--- servidorWeb/src/main/webapp/assets/scripts/consultaActividadDeportiva.js
+++ servidorWeb/src/main/webapp/assets/scripts/consultaActividadDeportiva.js
@@ -5,11 +5,11 @@ function altaCL(){
 		return false;
 	}
 	if (parseInt($("#minax").val()) > parseInt($("#asd").val())){
-		errorMsgForm("La cantidad máxima de cupos debe ser mayor a la mínima", "formulario-clase");
+		errorMsgForm("La cantidad máxima de cupos debe ser mayor o igual a la mínima ⚠", "formulario-clase");
 		return false;
 	}
 	if ((new Date($("#fechaIni").val())) < (new Date())){
-		errorMsgForm("No es posible dictar clases en el pasado.", "formulario-clase");
+		errorMsgForm("No es posible dictar clases en el pasado. ", "formulario-clase");
 		return false;
 	}
 	else
